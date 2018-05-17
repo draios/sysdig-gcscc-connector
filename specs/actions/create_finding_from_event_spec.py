311838e981ad23a68cf3b7c9c4bdfb4de4a80362
Néstor Salceda <nestor.salceda@sysdig.com>
2018-05-17 12:14:09 +0200

    Assert against finding instead of gcloud_client method call

--- specs/actions/create_finding_from_event_spec.py
+++ specs/actions/create_finding_from_event_spec.py
@@ -20,88 +20,71 @@ with description(securecscc.CreateFindingFromEvent) as self:
                                                         self.gcloud_client,
                                                         self.sysdig_client)
 
-    with it('allows on premise hosts for event url'):
-        self.action.run(fixtures.event())
+    with it('sends finding to Google Cloud Security Command Center'):
+        finding = self.action.run(fixtures.event())
 
         expect(self.gcloud_client.create_finding)\
-            .to(have_been_called_with(self.organization,
-                                      have_key('url', start_with(self.settings.sysdig_url_prefix()))))
+            .to(have_been_called_with(self.organization, finding))
+
+    with it('allows on premise hosts for event url'):
+        finding = self.action.run(fixtures.event())
+
+        expect(finding).to(have_key('url', start_with(self.settings.sysdig_url_prefix())))
 
     with it('extracts url path from security event'):
-        self.action.run(fixtures.event())
+        finding = self.action.run(fixtures.event())
 
-        expect(self.gcloud_client.create_finding)\
-            .to(have_been_called_with(self.organization,
-                                      have_key('url', end_with('/#/events/f:1523007251,t:1523007371/*/*?viewAs=list'))))
+        expect(finding).to(have_key('url', end_with('/#/events/f:1523007251,t:1523007371/*/*?viewAs=list')))
 
     with it('retrieves category name from sysdig client'):
         category_name = 'a category name'
         when(self.sysdig_client).find_policy_by_id(59).returns(category_name)
 
-        self.action.run(fixtures.event())
+        finding = self.action.run(fixtures.event())
 
-        expect(self.gcloud_client.create_finding)\
-            .to(have_been_called_with(self.organization,
-                                      have_key('category', category_name)))
+        expect(finding).to(have_key('category', category_name))
 
     with it('uses id from security event'):
-        self.action.run(fixtures.event())
+        finding = self.action.run(fixtures.event())
 
-        expect(self.gcloud_client.create_finding)\
-            .to(have_been_called_with(self.organization,
-                                      have_key('id', '530491201430929408')))
+        expect(finding).to(have_key('id', '530491201430929408'))
 
     with it('uses the source_id assigned to us from Google'):
-        self.action.run(fixtures.event())
+        finding = self.action.run(fixtures.event())
 
-        expect(self.gcloud_client.create_finding)\
-            .to(have_been_called_with(self.organization,
-                                      have_key('source_id', self.settings.source_id())))
+        expect(finding).to(have_key('source_id', self.settings.source_id()))
 
     with it('uses only seconds from event time'):
-        self.action.run(fixtures.event())
+        finding = self.action.run(fixtures.event())
 
-        expect(self.gcloud_client.create_finding)\
-            .to(have_been_called_with(self.organization,
-                                      have_key('event_time', 1523007311)))
+        expect(finding).to(have_key('event_time', 1523007311))
 
     with context('when building properties'):
         with it('adds output'):
             output = "Sensitive file opened for reading by non-trusted program (user=root program=ftest command=ftest -i 25200 -a exfiltration file=/etc/shadow parent=docker-containe gparent=docker-containe ggparent=dockerd gggparent=systemd)"
 
-            self.action.run(fixtures.event())
+            finding = self.action.run(fixtures.event())
 
-            expect(self.gcloud_client.create_finding)\
-                .to(have_been_called_with(self.organization,
-                                          have_key('properties',
-                                                   have_key('summary', output))))
+            expect(finding).to(have_key('properties', have_key('summary', output)))
 
         with it('adds severity'):
-            self.action.run(fixtures.event())
+            finding = self.action.run(fixtures.event())
 
-            expect(self.gcloud_client.create_finding)\
-                .to(have_been_called_with(self.organization,
-                                          have_key('properties',
-                                                   have_key('severity', 4))))
+            expect(finding).to(have_key('properties', have_key('severity', 4)))
 
         with it('adds rule type'):
-            self.action.run(fixtures.event())
+            finding = self.action.run(fixtures.event())
 
-            expect(self.gcloud_client.create_finding)\
-                .to(have_been_called_with(self.organization,
-                                          have_key('properties',
-                                                   have_key('rule.type', 'RULE_TYPE_FALCO'))))
+            expect(finding).to(have_key('properties', have_key('rule.type',
+                                                               'RULE_TYPE_FALCO')))
 
         with it('adds container metadata'):
             container_id = '57c1820a87f1'
             when(self.sysdig_client).find_container_metadata_from_container_id(container_id).returns({'container.stuff': 'FOO'})
 
-            self.action.run(fixtures.event())
+            finding = self.action.run(fixtures.event())
 
-            expect(self.gcloud_client.create_finding)\
-                .to(have_been_called_with(self.organization,
-                                          have_key('properties',
-                                                   have_key('container.stuff', 'FOO'))))
+            expect(finding).to(have_key('properties', have_key('container.stuff', 'FOO')))
 
     with context('when filling asset ids'):
         with context('when filling container image'):
@@ -110,26 +93,20 @@ with description(securecscc.CreateFindingFromEvent) as self:
 
             with it('queries sysdigcloud for its image id and adds to asset ids'):
                 container_image = 'gcr.io/google-containers/prometheus-to-sd@sha256:5831390762c790b0375c202579fd41dd5f40c71950f7538adbe14b0c16f35d56'
-
                 when(self.sysdig_client).find_container_image_from_container_id(self.container_id).returns(container_image)
 
-                self.action.run(fixtures.event())
+                finding = self.action.run(fixtures.event())
 
-                expect(self.gcloud_client.create_finding)\
-                    .to(have_been_called_with(self.organization,
-                                              have_key('asset_ids', [container_image])))
+                expect(finding).to(have_key('asset_ids', [container_image]))
 
             with context('and is not an image stored in Google Registry'):
                 with it('returns organization as asset id'):
                     container_image = 'hub.docker.com'
-
                     when(self.sysdig_client).find_container_image_from_container_id(self.container_id).returns(container_image)
 
-                    self.action.run(fixtures.event())
+                    finding = self.action.run(fixtures.event())
 
-                    expect(self.gcloud_client.create_finding)\
-                        .to(have_been_called_with(self.organization,
-                                                have_key('asset_ids', [self.organization])))
+                    expect(finding).to(have_key('asset_ids', [self.organization]))
 
         with context('when filling instance id'):
             with before.each:
@@ -142,21 +119,17 @@ with description(securecscc.CreateFindingFromEvent) as self:
                 when(self.sysdig_client).find_host_by_mac(self.mac).returns(self.hostname)
                 when(self.gcloud_client).get_instance_id_from_hostname(self.settings.project(), self.settings.zone(), self.hostname).returns(an_id)
 
-                self.action.run(fixtures.event())
+                finding = self.action.run(fixtures.event())
 
-                expect(self.gcloud_client.create_finding)\
-                    .to(have_been_called_with(self.organization,
-                                              have_key('asset_ids', [instance_image_id])))
+                expect(finding).to(have_key('asset_ids', [instance_image_id]))
 
             with context('and mac is not found on sysdig'):
                 with it('returns organization as asset id'):
                     when(self.sysdig_client).find_host_by_mac(self.mac).returns(None)
 
-                    self.action.run(fixtures.event())
+                    finding = self.action.run(fixtures.event())
 
-                    expect(self.gcloud_client.create_finding)\
-                        .to(have_been_called_with(self.organization,
-                                                  have_key('asset_ids', [self.organization])))
+                    expect(finding).to(have_key('asset_ids', [self.organization]))
 
             with context('and hostname is not found on google compute'):
                 with it('returns organization as asset id'):
@@ -167,11 +140,9 @@ with description(securecscc.CreateFindingFromEvent) as self:
                                                        self.settings.zone(),
                                                        self.hostname).returns(None)
 
-                    self.action.run(fixtures.event())
+                    finding = self.action.run(fixtures.event())
 
-                    expect(self.gcloud_client.create_finding)\
-                        .to(have_been_called_with(self.organization,
-                                                  have_key('asset_ids', [self.organization])))
+                    expect(finding).to(have_key('asset_ids', [self.organization]))
 
     with context('when receving a host event'):
         with before.each:
@@ -188,18 +159,14 @@ with description(securecscc.CreateFindingFromEvent) as self:
                                                                    self.settings.zone(),
                                                                    hostname).returns(an_id)
 
-            result = self.action.run(fixtures.event_host())
+            finding = self.action.run(fixtures.event_host())
 
-            expect(self.gcloud_client.create_finding)\
-                .to(have_been_called_with(self.organization,
-                                            have_key('asset_ids', [instance_image_id])))
+            expect(finding).to(have_key('asset_ids', [instance_image_id]))
 
         with it('does not add any container metadata'):
-            self.action.run(fixtures.event())
+            finding = self.action.run(fixtures.event())
 
-            expect(self.gcloud_client.create_finding)\
-                .to(have_been_called_with(self.organization,
-                                          have_key('properties', have_keys('summary', 'severity', 'rule.type'))))
+            expect(finding).to(have_key('properties', have_keys('summary', 'severity', 'rule.type')))
 
     with context('when creating from falco'):
         with it('uses the source_id assigned to us from Google'):
