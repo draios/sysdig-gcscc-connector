24815be4fa544a9134214a5f73f135343d31ab46
Néstor Salceda <nestor.salceda@sysdig.com>
2018-05-18 12:30:20 +0200

    Inject required finding mapper instead of choosing at runtime

--- securecscc/actions.py
+++ securecscc/actions.py
@@ -1,28 +1,14 @@
-from securecscc import finding_mappers
-
-
 class CreateFindingFromEvent:
-    def __init__(self, settings, gcloud_client, sysdig_client):
+    def __init__(self, settings, gcloud_client, finding_mapper):
         self._settings = settings
         self._gcloud_client = gcloud_client
-        self._sysdig_client = sysdig_client
-
-        self._sysdig_secure_finding_mapper = finding_mappers.SysdigSecureFindingMapper(self._settings, self._sysdig_client, self._gcloud_client)
-        self._falco_finding_mapper = finding_mappers.FalcoFindingMapper(self._settings)
+        self._finding_mapper = finding_mapper
 
     def run(self, event):
-        if self._comes_from_sysdig_secure(event):
-            finding = self._sysdig_secure_finding_mapper.create_from(event)
-        else:
-            finding = self._falco_finding_mapper.create_from(event)
-
-        self._gcloud_client.create_finding(self._settings.organization(),
-                                           finding)
+        finding = self._finding_mapper.create_from(event)
+        self._gcloud_client.create_finding(self._settings.organization(), finding)
 
         return finding
-
-    def _comes_from_sysdig_secure(self, event):
-        return 'version' in event
 
 
 class CreateCSCCNotificationChannel:
--- specs/actions/create_finding_from_event_spec.py
+++ specs/actions/create_finding_from_event_spec.py
@@ -1,6 +1,6 @@
 from mamba import description, it, before
 from expects import expect
-from doublex import Spy
+from doublex import Spy, Stub, when
 from doublex_expects import have_been_called_with
 
 import securecscc
@@ -11,17 +11,18 @@ from specs.support import fixtures
 with description(securecscc.CreateFindingFromEvent) as self:
     with before.each:
         self.settings = securecscc.Settings()
-        self.organization = self.settings.organization()
-
         self.gcloud_client = Spy(securecscc.GoogleCloudClient)
-        self.sysdig_client = Spy(securecscc.SysdigSecureClient)
+        self.finding_mapper = Stub(securecscc.FalcoFindingMapper)
 
         self.action = securecscc.CreateFindingFromEvent(self.settings,
                                                         self.gcloud_client,
-                                                        self.sysdig_client)
+                                                        self.finding_mapper)
 
-    with it('sends finding to Google Cloud Security Command Center'):
-        finding = self.action.run(fixtures.event())
+    with it('sends parsed finding to Google Cloud Security Command Center'):
+        finding = 'irrelevant finding'
+        when(self.finding_mapper).create_from(fixtures.event()).returns(finding)
+
+        self.action.run(fixtures.event())
 
         expect(self.gcloud_client.create_finding)\
-            .to(have_been_called_with(self.organization, finding))
+            .to(have_been_called_with(self.settings.organization(), finding))
