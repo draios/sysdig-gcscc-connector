022c346057e88e4f510f25d128176a7b92f7258e
Néstor Salceda <nestor.salceda@gmail.com>
2019-06-24 18:18:34 +0200

    Ensure that gunicron writes log to STDOUT and STDERR

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,11 +7,11 @@ if [ "$1" = 'sysdig_secure_poller' ]; then
 fi
 
 if [ "$1" = 'sysdig_secure_webhook' ]; then
-    exec gunicorn -b 0.0.0.0:8080 connectors.sysdig_secure_webhook:app
+    exec gunicorn -b 0.0.0.0:8080 --access-logfile - connectors.sysdig_secure_webhook:app
 fi
 
 if [ "$1" = 'falco_webhook' ]; then
-    exec gunicorn -b 0.0.0.0:8080 connectors.falco_webhook:app
+    exec gunicorn -b 0.0.0.0:8080 --access-logfile - connectors.falco_webhook:app
 fi
 
 if [ "$1" = 'create_notification_channel' ]; then
